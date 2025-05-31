f255c64b0b5010990dcb45679ff55126521f2b5a
FreddyOkino <freddyokino@hotmail.com>
2025-05-31 14:34:17 -0400

    [card 3] Criando a pagina de login

---
+++ src/view/LoginView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div >
+    <h2 >Login</h2>
+    <form @submit.prevent="onSubmit">
+      <div class="mb-4">
+        <label for="email" class="block text-gray-700">Email</label>
+        <input
+          v-model="email"
+          type="email"
+          id="email"
+          required
+
+        />
+      </div>
+
+      <div class="mb-6">
+        <label for="password" class="block text-gray-700">Senha</label>
+        <input
+          v-model="password"
+          type="password"
+          id="password"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+      >
+        Entrar
+      </button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const email = ref<string>('');
+const password = ref<string>('');
+
+
+const emit = defineEmits<{
+  (e: 'login', payload: { email: string; password: string }): void;
+}>();
+
+function onSubmit() {
+  emit('login', { email: email.value, password: password.value });
+}
+</script>
+
+<style scoped>
+
+</style>
