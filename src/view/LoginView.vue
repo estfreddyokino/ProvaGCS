d78c5b7fd65358a30a6c834f6b24912757159e35
FreddyOkino <freddyokino@hotmail.com>
2025-05-31 15:15:11 -0400

    [card 6] criando banco de dados para simular o login

--- src/view/LoginView.vue
+++ src/view/LoginView.vue
@@ -1,57 +1,53 @@
 <template>
-  <div >
-    <h2 >Login</h2>
-    <form @submit.prevent="onSubmit">
-      <div class="mb-4">
-        <label for="email" class="block text-gray-700">Email</label>
-        <input
-          v-model="email"
-          type="email"
-          id="email"
-          required
+    <div>
+        <h2>Login</h2>
+        <form @submit.prevent="onSubmit">
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700">Email</label>
+                <input v-model="email" type="email" id="email" required />
+            </div>
 
-        />
-      </div>
+            <div class="mb-6">
+                <label for="password" class="block text-gray-700">Senha</label>
+                <input v-model="password" type="password" id="password" required />
+            </div>
 
-      <div class="mb-6">
-        <label for="password" class="block text-gray-700">Senha</label>
-        <input
-          v-model="password"
-          type="password"
-          id="password"
-          required
-        />
-      </div>
-
-      <button
-        type="submit"
-      >
-        Entrar
-      </button>
-    </form>
-  </div>
+            <button type="submit">
+                Entrar
+            </button>
+        </form>
+    </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { users } from '../database/users';
 
 const email = ref<string>('');
 const password = ref<string>('');
 
-
 const emit = defineEmits<{
-  (e: 'login', payload: { email: string; password: string }): void;
+    (e: 'login', payload: { email: string; password: string }): void;
 }>();
 
 function onSubmit() {
-  if (!email.value || !password.value) {
-    alert('Preencha todos os campos!');
-    return;
-  }
-  emit('login', { email: email.value, password: password.value });
+    if (!email.value || !password.value) {
+        alert('Preencha todos os campos!');
+        return;
+    }
+
+    const userExists = users.find(
+        (user) => user.email === email.value && user.password === password.value
+    );
+
+    if (!userExists) {
+        alert('Usuário ou senha inválidos!');
+        return;
+    }
+
+    emit('login', { email: email.value, password: password.value });
 }
 </script>
 
-<style scoped>
 
-</style>
+<style scoped></style>
